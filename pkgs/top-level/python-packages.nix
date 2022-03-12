8bca5181805d32f3c798eaeb2c56310b31a53afa
Fabian Affolter <mail@fabian-affolter.ch>
2022-03-12 11:34:11 +0100

    python3Packages.pysigma: init at 0.3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7637,6 +7637,8 @@ in {
 
   pysideTools = callPackage ../development/python-modules/pyside/tools.nix { };
 
+  pysigma = callPackage ../development/python-modules/pysigma { };
+
   pysignalclirestapi = callPackage ../development/python-modules/pysignalclirestapi { };
 
   pysigset = callPackage ../development/python-modules/pysigset { };

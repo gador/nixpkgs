df487b15ffb4fd4958fd435412f49f0c3803bfb4
AndersonTorres <torres.anderson.85@protonmail.com>
2022-03-13 19:06:09 -0300

    fingerd_bsd: "merge" with finger_bsd
    In order to reduce duplicated code

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5483,6 +5483,7 @@ with pkgs;
   findutils = callPackage ../tools/misc/findutils { };
 
   finger_bsd = callPackage ../tools/networking/bsd-finger { };
+  fingerd_bsd = finger_bsd.override({ buildClient = false; });
 
   iprange = callPackage ../applications/networking/firehol/iprange.nix {};
 
@@ -21272,8 +21273,6 @@ with pkgs;
   felix = callPackage ../servers/felix { };
 
   felix_remoteshell = callPackage ../servers/felix/remoteshell.nix { };
-
-  fingerd_bsd = callPackage ../servers/fingerd/bsd-fingerd { };
 
   inherit (callPackages ../servers/firebird { }) firebird_4 firebird_3 firebird_2_5 firebird;
 

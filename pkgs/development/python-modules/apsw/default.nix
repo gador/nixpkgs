80b6f7aa318af5b16194709f956f1cd75f10561e
Austin Butler <abutler@riotgames.com>
2022-04-11 18:56:47 +0000

    python3Packages.apsw: 3.37.0-r1 -> 3.38.1-r1

--- pkgs/development/python-modules/apsw/default.nix
+++ pkgs/development/python-modules/apsw/default.nix
@@ -4,7 +4,6 @@
 , fetchFromGitHub
 , sqlite
 , isPyPy
-, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -18,15 +17,11 @@ buildPythonPackage rec {
     owner = "rogerbinns";
     repo = "apsw";
     rev = version;
-    hash = "sha256-pbb6wCu1T1mPlgoydB1Y1AKv+kToGkdVUjiom2vTqf4=";
+    sha256 = "sha256-pbb6wCu1T1mPlgoydB1Y1AKv+kToGkdVUjiom2vTqf4=";
   };
 
   buildInputs = [
     sqlite
-  ];
-
-  checkInputs = [
-    pytestCheckHook
   ];
 
   # Works around the following error by dropping the call to that function
@@ -37,24 +32,12 @@ buildPythonPackage rec {
     sed -i '/print_version_info(write)/d' tests.py
   '';
 
-  pytestFlagsArray = [
-    "tests.py"
-  ];
-
-  disabledTests = [
-    "testCursor"
-    "testdb"
-    "testLargeObjects"
-    "testLoadExtension"
-    "testShell"
-    "testVFS"
-    "testVFSWithWAL"
-  ] ++ lib.optionals stdenv.isDarwin [
-    # This is https://github.com/rogerbinns/apsw/issues/277 but
-    # because we use pytestCheckHook we need to blacklist the test
-    # manually
-    "testzzForkChecker"
-  ];
+  # Project uses custom test setup to exclude some tests by default, so using pytest
+  # requires more maintenance
+  # https://github.com/rogerbinns/apsw/issues/335
+  checkPhase = ''
+    python setup.py test
+  '';
 
   pythonImportsCheck = [
     "apsw"

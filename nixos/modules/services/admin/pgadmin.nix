9018d15a6fd0da30e8cc91c87f7af1de21c34f33
Florian Brandes <florian.brandes@posteo.de>
2022-08-27 23:37:28 +0200

    pgadmin4: 6.12 -> 6.13
    - Add update script
    - Add email options to pgadmin4 nixOS module
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- nixos/modules/services/admin/pgadmin.nix
+++ nixos/modules/services/admin/pgadmin.nix
@@ -31,7 +31,7 @@ in
     enable = mkEnableOption (lib.mdDoc "PostgreSQL Admin 4");
 
     port = mkOption {
-      description = lib.mdDoc "Port for pgadmin4 to run on";
+      description = lib.mdDoc "Port for pgadmin4 to run on.";
       type = types.port;
       default = 5050;
     };
@@ -49,6 +49,55 @@ in
       type = types.path;
     };
 
+    emailServer = {
+      enable = mkOption {
+        description = lib.mdDoc ''
+          Enable SMTP email server. This is necessary, if you want to use password recovery or change your own password.
+        '';
+        type = types.bool;
+        default = false;
+      };
+      address = mkOption {
+        description = lib.mdDoc "SMTP server for email delivery.";
+        type = types.str;
+        default = "localhost";
+      };
+      port = mkOption {
+        description = lib.mdDoc "SMTP server port for email delivery.";
+        type = types.port;
+        default = 25;
+      };
+      useSSL = mkOption {
+        description = lib.mdDoc "SMTP server should use SSL.";
+        type = types.bool;
+        default = false;
+      };
+      useTLS = mkOption {
+        description = lib.mdDoc "SMTP server should use TLS.";
+        type = types.bool;
+        default = false;
+      };
+      username = mkOption {
+        description = lib.mdDoc "SMTP server username for email delivery.";
+        type = types.str;
+        default = null;
+      };
+      sender = mkOption {
+        description = lib.mdDoc ''
+          SMTP server sender email for email delivery. Some servers require this to be a valid email address from that server.
+        '';
+        type = types.str;
+        example = "noreply@example.com";
+      };
+      passwordFile = mkOption {
+        description = lib.mdDoc ''
+          Password for SMTP email account.
+          NOTE: Should be string not a store path, to prevent the password from being world readable.
+        '';
+        type = types.path;
+      };
+    };
+
     openFirewall = mkEnableOption (lib.mdDoc "firewall passthrough for pgadmin4");
 
     settings = mkOption {
@@ -57,7 +106,7 @@ in
         [Documentation](https://www.pgadmin.org/docs/pgadmin4/development/config_py.html).
       '';
       type = pyType;
-      default= {};
+      default = { };
     };
   };
 
@@ -69,6 +118,13 @@ in
       SERVER_MODE = true;
     } // (optionalAttrs cfg.openFirewall {
       DEFAULT_SERVER = mkDefault "::";
+    }) // (optionalAttrs cfg.emailServer.enable {
+      MAIL_SERVER = cfg.emailServer.address;
+      MAIL_PORT = cfg.emailServer.port;
+      MAIL_USE_SSL = cfg.emailServer.useSSL;
+      MAIL_USE_TLS = cfg.emailServer.useTLS;
+      MAIL_USERNAME = cfg.emailServer.username;
+      SECURITY_EMAIL_SENDER = cfg.emailServer.sender;
     });
 
     systemd.services.pgadmin = {
@@ -115,10 +171,15 @@ in
       group = "pgadmin";
     };
 
-    users.groups.pgadmin = {};
+    users.groups.pgadmin = { };
 
     environment.etc."pgadmin/config_system.py" = {
-      text = formatPy cfg.settings;
+      text =
+        (if cfg.emailServer.enable then ''
+          with open("${cfg.emailServer.passwordFile}") as f:
+            pw = f.read()
+          MAIL_PASSWORD = pw
+        '' else "") + formatPy cfg.settings;
       mode = "0600";
       user = "pgadmin";
       group = "pgadmin";

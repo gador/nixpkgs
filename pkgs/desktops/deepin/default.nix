87966a5bcee2c574b5c26c1eaa96f078dd9bc4f0
rewine <luhongxu@deepin.org>
2023-01-09 18:09:01 +0800

    go-gir-generator: init at 2.2.0

--- pkgs/desktops/deepin/default.nix
+++ pkgs/desktops/deepin/default.nix
@@ -41,6 +41,7 @@ let
 
     #### Go Packages
     go-lib = callPackage ./go-package/go-lib { inherit replaceAll; };
+    go-gir-generator = callPackage ./go-package/go-gir-generator { };
 
     #### ARTWORK
     dde-account-faces = callPackage ./artwork/dde-account-faces { };

f3bd22363785fbc83624921d15b9e45fe2c7de1e
Florian Brandes <florian.brandes@posteo.de>
2024-04-23 15:06:43 +0200

    nixos/blenderfarm: init
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -699,6 +699,7 @@
   ./services/misc/beanstalkd.nix
   ./services/misc/bees.nix
   ./services/misc/bepasty.nix
+  ./services/misc/blenderfarm.nix
   ./services/misc/calibre-server.nix
   ./services/misc/canto-daemon.nix
   ./services/misc/cfdyndns.nix

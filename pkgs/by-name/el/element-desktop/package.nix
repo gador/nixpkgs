530ebfc7826dd36e82d0e5b712b5edb8cc8c42fc
Florian Brandes <florian.brandes@posteo.de>
2026-02-22 00:08:37 +0100

    Revert "element-desktop: 1.12.9 -> 1.12.10"
    This reverts commit 56fa80e0eea40a6dcd8e3a0d7e08229b831812d0.

--- pkgs/by-name/el/element-desktop/package.nix
+++ pkgs/by-name/el/element-desktop/package.nix
@@ -5,7 +5,7 @@
   makeWrapper,
   makeDesktopItem,
   nodejs,
-  electron_40,
+  electron_39,
   element-web,
   sqlcipher,
   callPackage,
@@ -24,7 +24,7 @@ let
   pinData = import ./element-desktop-pin.nix;
   inherit (pinData.hashes) desktopSrcHash desktopYarnHash;
   executableName = "element-desktop";
-  electron = electron_40;
+  electron = electron_39;
   seshat = callPackage ./seshat { };
 in
 stdenv.mkDerivation (
